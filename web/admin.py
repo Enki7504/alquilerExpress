6b6844056618eed8d444859fc56c0cb0a19b75c1
Enki7504 <111676999+Enki7504@users.noreply.github.com>
2025-04-28 13:21:29 -0300

    tablas de ejemplo creadas

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-
-
 # Register your models here.
+from .models import Cliente, Propiedad
+
+admin.site.register(Cliente)
+admin.site.register(Propiedad)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Propiedad(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
