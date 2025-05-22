5999d56eefeae55211aae2583b4d50fdf1a72170
PatricioTomasGarcia12 <patricio.garcia.tomas@gmail.com>
2025-05-21 22:44:30 -0300

    inicio y cierre de sesion. ver que onda con la base de datos y el inicio :(

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/',  LoginView.as_view(
+        template_name='login.html',
+        redirect_authenticated_user=True
+        ),                            name='login'),
+    path('logout/',  views.logout_view, name='logout'),
 ]
+
--- web/views.py
+++ web/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+def logout_view(request):
+    """Sólo procesa POST para cerrar sesión y redirige a login"""
+    if request.method == 'POST':
+        logout(request)
+        messages.success(request, "Has cerrado sesión correctamente.")
+        return redirect('login')
+    # si alguien entra por GET, lo mandamos al index
+    return redirect('index')
+
